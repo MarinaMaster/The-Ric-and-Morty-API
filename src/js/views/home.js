aa4843e0ec1f93065c695c3fbf7cf7ffe9bf80be
Marina Master <marina777master@mail.ru>
2023-09-18 19:47:48 -0400

    added Cards and Cards.Styles

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,41 @@
-import React from "react";
+import React,{useState, useEffect} from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
+import Filters from "../component/Filters/Filters";
+import Cards from "../component/Cards/Cards";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+export const Home = () => {
+	
+	const [pageNumber, setPageNumber] = useState(1);
+	const [fetchedData, updateFetchedData]=useState([]);
+	const {info,results} = fetchedData;
+	
+	let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}`;
+
+	useEffect(() => {
+		(async function () {
+			let data = await fetch(api).then((res)=>res.json());
+			updateFetchedData(data);
+	})();
+	}, [api]);
+
+	return (
+	<div className="App">
+		<h1 className="text-center ubuntu my-4">
+		Rick & Morty <span className="text-primary">WiKi</span></h1>
+
+		<div className="container">
+			<div className="row">
+				<div className="col-3"><Filters/></div>
+				<div className="col-8">
+				<div className="row">
+					<Cards results={results}/>
+					
+				</div>
+				</div>
+			</div>
+		</div>
+	</div> )
+};
